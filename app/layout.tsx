238ed8f0631a58286fdbe46e577996352d56596f
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-04-21 19:51:10 +0500

    fonts through tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import "./globals.css";
 
+import localFont from "next/font/local";
+import { Red_Hat_Display } from "next/font/google";
+import { Merriweather } from "next/font/google";
+import { Merriweather_Sans } from "next/font/google";
+
+// FONTS
+const aeroport = localFont({
+  src: "../fonts/Aeroport_Bold.otf",
+  variable: "--font-aeroport",
+});
+const redhat = Red_Hat_Display({
+  subsets: ["latin", "latin-ext"],
+  variable: "--font-redhat",
+});
+const mw = Merriweather({
+  subsets: ["cyrillic", "cyrillic-ext", "latin", "latin-ext"],
+  weight: "700",
+  variable: "--font-mw",
+});
+const mw_sans = Merriweather_Sans({
+  subsets: ["cyrillic-ext", "latin", "latin-ext"],
+  variable: "--font-mwsans",
+});
+
 export const metadata = {
   title: "Turkmen TV",
 };
@@ -10,7 +34,10 @@ interface IProps {
 
 export default function RootLayout({ children }: IProps) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${aeroport.variable} ${mw.variable} ${redhat.variable} ${mw_sans.variable}`}
+    >
       <body>{children}</body>
     </html>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
-const { fontFamily } = require("tailwindcss/defaultTheme");
+import { fontFamily } from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        aeroport: ["var(--font-aeroport)", ...fontFamily.sans],
-        redhat: ["var(--font-redhat)", ...fontFamily.sans],
-        mw: ["var(--font-mw)", ...fontFamily.sans],
-        mw_sans: ["var(--font-mwsans)", ...fontFamily.sans],
-      },
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}",
+];
+export const darkMode = "class";
+export const theme = {
+  extend: {
+    fontFamily: {
+      aeroport: ["var(--font-aeroport)", ...fontFamily.sans],
+      redhat: ["var(--font-redhat)", ...fontFamily.sans],
+      mw: ["var(--font-mw)", ...fontFamily.sans],
+      mw_sans: ["var(--font-mwsans)", ...fontFamily.sans],
     },
   },
-  plugins: [],
 };
+export const plugins = [];
