d7abaf30abbb0137966086d0f240ab95b8289e54
Aydogdy Agabayev <76523543+VividTruthKeeper@users.noreply.github.com>
2023-04-27 18:07:52 +0300

    Update Aside.tsx

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { ISource } from '../typings/source.type';
 import Image from 'next/image';
 
 const Aside = () => {
-  const sample: ISource[] = [
+  const samples: ISource[] = [
     { id: 1, img: '/video_source_1.jpg', name: 'Ashgabat' },
     { id: 2, img: '/video_source_2.jpg', name: 'Altyn Asyr' },
     { id: 3, img: '/video_source_3.jpg', name: 'Radio' },
@@ -20,7 +19,7 @@ const Aside = () => {
 
   return (
     <aside className="aside flex flex-col gap-3 pr-7 border border-t-0 border-l-0 border-b-0 border-r-gray-400 h-fit w-fit pb-5">
-      {sample.map((image) => (
+      {samples.map((image) => (
         <div
           className="aside-img w-14 h-14 rounded-full overflow-hidden border py-3 px-2 bg-white border-gray-500"
           key={image.id}>
